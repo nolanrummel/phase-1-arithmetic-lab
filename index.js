1c3d20f3e25977ee8e11110334f792ff12225958
Nolan Rummel <nolan.rummel22@gmail.com>
2023-05-24 14:38:10 -0400

    complete lab

--- index.js
+++ index.js
@@ -1,2 +1,11 @@
+let num1 = 2;
+let num2 = 31;
+const multiply = num1 * num2;
 
-// Write your code here
+const random = Math.round(Math.random() * 10)
+
+let num3 = 16
+let num4 = 4
+const mod = num3 / num4
+
+const max = Math.max(1, 2, 3, 20)
